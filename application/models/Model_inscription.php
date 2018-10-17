22ab2e4d912c8a61ba6b183b3d8422f54d859e18
Arnaud Soukhavong <asoukhav@live.fr>
2018-10-17 10:08:24 +0200

    Modele de la page inscription

--- application/models/Model_inscription.php
+++ application/models/Model_inscription.php
@@ -5,7 +5,7 @@ class Model_inscription extends CI_Model
     public function GetAllInscription()
     {
         $sql = $this->db->query('INSERT INTO user (nomUser, login, mdp)
-        VALUES ('.$nomUser.', '.$login.', ' .$passe.')');
+        VALUES ('.$_POST['nomUser'].', '.$_POST['login'].', ' .$_POST['passe'].')');
 
         return $sql->result();
 
