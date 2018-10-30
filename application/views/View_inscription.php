a9c400af7bdb0d3e5057c0d5d3b2a1cb10bc5b21
Terranos7 <arthurB77@free.fr>
2018-10-30 17:58:05 +0100

    correctionbug et ajout css

--- application/views/View_inscription.php
+++ application/views/View_inscription.php
@@ -4,17 +4,20 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Inscription</title>
-    <link rel="stylesheet" href="Bootstrap/css/bootstrap.css" />
-    <script type="text/javascript" src="JQuery/jquery-3.1.1.min.js"></script>
-    <script type="text/javascript" src="JS/MesFonctions.js"></script>
+    <link rel="stylesheet" href="<?php echo base_url();?>Bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>CSS/StyleInscription.css">
+    <script type="text/javascript" src="<?php echo base_url();?>JQuery/jquery-3.1.1.js"></script>
+    <script type="text/javascript" src="<?php echo base_url();?>JS/MesFonctions.js"></script>
 </head>
 <body>
     <h1>Inscription</h1><br>
+    <p>
     <?php
     echo "<label>Nom</label> <input type='text' id='txtNom'><br><br>";
     echo "<label>Login</label> <input type='text' id='txtLogin'><br><br>";
     echo "<label>Mot de passe</label> <input type='password' id='txtMdp'><br><br>";
-    echo "<input type='button' value='Créer un compte' onclick='Ajoutinscription()'>"
+    echo "<input type='button' id='boutoniscription' value='Créer un compte' onclick='Ajoutinscription()'>"
     ?>
+    </p>
     </body>
 </html>
